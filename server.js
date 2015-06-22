30b704a45c31f129844c85007ed7e46a3531440a
Gregg Hanano <gregg.hanano@gmail.com>
2015-06-22 14:27:50 -0700

    changed porting

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 8000));
+
 app.use(express.static(path.join(__dirname, './client')));
 
-app.listen(8000, function(){
-	console.log('Listening on port 8000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
